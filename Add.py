7158e9f72211d72bed70815baa582be4417c2a29
MightySpaceman <spacemanmcspace40@gmail.com>
2022-12-12 19:08:44 +1100

    e

--- Add.py
+++ Add.py
@@ -1,9 +1,5 @@
-Firstnum = input("First ")
-Secondnum = input("Second ")
-Output = int(Firstnum) + int(Secondnum)
-print("Output: " + str(Output))
-Exit = input("Do you want to exit the program y/n ")
-if Exit == "n":
-    exec(open('main.py').read())
-else:
-    exit
+def add():
+    Firstnum = input("First ")
+    Secondnum = input("Second ")
+    Output = int(Firstnum) + int(Secondnum)
+    print("Output: " + str(Output))
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-print("Input The Operation You want to run(Add = + Subtraction = - Multiplication = * Divistion = / )")
-Op = input("Operation: ")
-if Op == "+":
-    exec(open('Add.py').read())
-if Op == "-":
-    exec(open('Sub.py').read())
-if Op == "*":
-    exec(open('Mul.py').read())
-if Op == "/":
-    exec(open('Div.py').read())
+from Add import *
+from Sub import *
+from Mul import *
+from Div import *
+
+while True:
+    print("Input The Operation You want to run(Add = + Subtraction = - Multiplication = * Divistion = / )")
+    Op = input("Operation: ")
+    
+    if Op == "+":
+        add()
+    if Op == "-":
+        sub()
+    if Op == "*":
+        mul()
+    if Op == "/":
+        div()
